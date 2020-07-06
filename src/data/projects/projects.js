cbd767ae5d2d30e7072b4e9f34a2f8f492054e75
Bobby Gifford <bobby077joe@gmail.com>
2020-07-06 14:53:38 -0700

    Added data

--- src/data/projects/projects.js
+++ src/data/projects/projects.js
@@ -1,85 +1,95 @@
 const VerizonMedia = {
-  img: "https://esoteric-social-pics.s3-us-west-2.amazonaws.com/vzmedia.png",
-  title: "Verizon Media",
+  img: 'https://esoteric-social-pics.s3-us-west-2.amazonaws.com/vzmedia.png',
+  title: 'Verizon Media',
   description:
-    "A marketing and advertising site for Verizon Media. Availible to over 20 different countries and markets.",
-  tech: ["React", "Node", "Express", "PHP", "Drupal", "AWS"],
-  link: "https://verizonmedia.com"
+    'A marketing and advertising site for Verizon Media. Availible to over 20 different countries and markets.',
+  tech: ['React', 'Node', 'Express', 'PHP', 'Drupal', 'AWS'],
+  link: 'https://verizonmedia.com',
 };
 
 const VerizonCreative = {
   img:
-    "https://esoteric-social-pics.s3-us-west-2.amazonaws.com/verizoncreative.png",
-  title: "Verizon Creative",
+    'https://esoteric-social-pics.s3-us-west-2.amazonaws.com/verizoncreative.png',
+  title: 'Verizon Creative',
   description:
-    "A site dedicatied to current and future Verizon media employees.",
-  tech: ["HTML", "CSS", "JavaScript"],
-  link: "https://creative.verizonmedia.com"
+    'A site dedicatied to current and future Verizon media employees.',
+  tech: ['HTML', 'CSS', 'JavaScript'],
+  link: 'https://creative.verizonmedia.com',
+};
+
+const YahooDownloads = {
+  img:
+    'https://esoteric-social-pics.s3-us-west-2.amazonaws.com/download-yahoo.png',
+  title: 'Yahoo! Downloads',
+  description:
+    'A hub that displays all current availible yahoo browser extensions.',
+  tech: ['Gatsby', 'React', 'Bootstrap', 'AWS'],
+  link: 'https://downloads.yahoo.com/',
 };
 
 const Epixego = {
-  img: "https://esoteric-social-pics.s3-us-west-2.amazonaws.com/epixego.png",
-  title: "Epixego",
+  img: 'https://esoteric-social-pics.s3-us-west-2.amazonaws.com/epixego.png',
+  title: 'Epixego',
   description:
-    "A skills genome knowledge graph that is able to mimic the neurological connections of the human brain. It uses information about context unavailable through typically resumes.",
-  tech: ["React", "Node", "Express", "ElasticSearch", "Bootstrap"],
-  link: "https://epixego.com/"
+    'A skills genome knowledge graph that is able to mimic the neurological connections of the human brain. It uses information about context unavailable through typically resumes.',
+  tech: ['React', 'Node', 'Express', 'ElasticSearch', 'Bootstrap'],
+  link: 'https://epixego.com/',
 };
 
 const YahooElection = {
-  img: "https://esoteric-social-pics.s3-us-west-2.amazonaws.com/2020e.png",
-  title: "Yahoo Elections",
+  img: 'https://esoteric-social-pics.s3-us-west-2.amazonaws.com/2020e.png',
+  title: 'Yahoo Elections',
   description:
     "Yahoo's source for their political blogs and info for the current cycle.",
-  tech: ["HTML", "CSS", "JavaScript"],
-  link: "https://www.yahoo.com/news/elections"
+  tech: ['HTML', 'CSS', 'JavaScript'],
 };
 
 const SerapisSociety = {
   img:
-    "https://esoteric-social-pics.s3-us-west-2.amazonaws.com/serapis-social.png",
-  title: "Serapis Society",
+    'https://esoteric-social-pics.s3-us-west-2.amazonaws.com/serapis-social.png',
+  title: 'Serapis Society',
   description:
-    "A social network, Twitter like, application that is targeted toward niche interests in the scientific community.",
-  tech: ["React", "Redux", "Material UI", "Firebase"],
+    'A social network, Twitter like, application that is targeted toward niche interests in the scientific community.',
+  tech: ['React', 'Redux', 'Material UI', 'Firebase'],
 };
 
 const LostAncient = {
-  img: "https://esoteric-social-pics.s3-us-west-2.amazonaws.com/lost.png",
-  title: "Lost Ancient Knowledge",
+  img: 'https://esoteric-social-pics.s3-us-west-2.amazonaws.com/lost.png',
+  title: 'Lost Ancient Knowledge',
   description:
-    "A fullstack social media app dedicated to the spread and awareness of ancient sites.Its purpose is to spread knowledge among ancient architect enthusiasts.",
-  tech: ["Python", "Djano", "Postgres", "Digital Ocean", "NGINX", "Gunicorn"],
+    'A fullstack social media app dedicated to the spread and awareness of ancient sites.Its purpose is to spread knowledge among ancient architect enthusiasts.',
+  tech: ['Python', 'Djano', 'Postgres', 'Digital Ocean', 'NGINX', 'Gunicorn'],
 };
 
 const LadyCoffee = {
   img:
-    "https://esoteric-social-pics.s3-us-west-2.amazonaws.com/screenshot-lady-coffee.netlify.com-2019.07.21-18_59_34.png",
-  title: "Lady Coffee",
+    'https://esoteric-social-pics.s3-us-west-2.amazonaws.com/screenshot-lady-coffee.netlify.com-2019.07.21-18_59_34.png',
+  title: 'Lady Coffee',
   description:
-    "A full ecommerce app with cart and CMS capabilities. The client was very particular about fair trade use with their products. The client can access their products with contentful.",
-  tech: ["React", "Gatsby JS", "Contentful", "Netlify"],
+    'A full ecommerce app with cart and CMS capabilities. The client was very particular about fair trade use with their products. The client can access their products with contentful.',
+  tech: ['React', 'Gatsby JS', 'Contentful', 'Netlify'],
 };
 
 const AncientArchitecture = {
-  img: "https://esoteric-social-pics.s3-us-west-2.amazonaws.com/AA2.png",
-  title: "Ancient Architecture",
+  img: 'https://esoteric-social-pics.s3-us-west-2.amazonaws.com/AA2.png',
+  title: 'Ancient Architecture',
   description:
-    "A social media app dedicated to spreading news of ancient architecture.",
-  tech: ["React", "Redux", "Node", "Express", "MongoDB", "Bootstrap"]
+    'A social media app dedicated to spreading news of ancient architecture.',
+  tech: ['React', 'Redux', 'Node', 'Express', 'MongoDB', 'Bootstrap'],
 };
 
 const KukusCorner = {
   img:
-    "https://esoteric-social-pics.s3-us-west-2.amazonaws.com/kukuscorner2.png",
+    'https://esoteric-social-pics.s3-us-west-2.amazonaws.com/kukuscorner2.png',
   title: "Kuku's Corner",
   description:
-    "A full stack e-commerce app made for a small independent bakery. I worked on this while at Drishticon. I was commissioned to lead migration of the front end from Angular 1 to React. The back-end was written with Java and Spring Boot.",
-  tech: ["React", "Redux", "Angular", "Bootstrap"]
+    'A full stack e-commerce app made for a small independent bakery. I worked on this while at Drishticon. I was commissioned to lead migration of the front end from Angular 1 to React. The back-end was written with Java and Spring Boot.',
+  tech: ['React', 'Redux', 'Angular', 'Bootstrap'],
 };
 
 export const ProjectData = [
   VerizonMedia,
+  YahooDownloads,
   YahooElection,
   VerizonCreative,
   SerapisSociety,
@@ -87,5 +97,5 @@ export const ProjectData = [
   Epixego,
   LadyCoffee,
   AncientArchitecture,
-  KukusCorner
+  KukusCorner,
 ];
