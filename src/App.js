f337093595189b4527ee1d80d23704f794ba8d40
Bobby Gifford <bobby077joe@gmail.com>
2020-07-11 15:35:52 -0700

    Added router, helmet, and seo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Home } from './Pages/Home/Home';
+import { Router, Route } from 'react-router-dom';
+import history from './history';
 
 function App() {
   return (
     <>
-      <Home />
+      <Router history={history}>
+        <Route exact path='/' component={Home} />
+      </Router>
     </>
   );
 }
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { Container } from 'react-bootstrap';
 import { Feature } from '../../Components/Feature/Feature';
 import { Projects } from '../Projects/Projects';
@@ -6,6 +7,14 @@ import { Projects } from '../Projects/Projects';
 export function Home() {
   return (
     <>
+      <Helmet>
+        <title>{"Bobby's Resume | Home"}</title>
+        <meta
+          name='description'
+          content={"This is Bobby Gifford's personal resume website."}
+        />
+        {/* <meta name='og:image' content={} /> */}
+      </Helmet>
       <Container>
         <Feature />
       </Container>
